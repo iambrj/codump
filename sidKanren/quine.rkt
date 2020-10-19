986048b2346ee557bbdc2711990c94d561024828
iambrj <joshibharathiramana@gmail.com>
2020-10-19 17:03:47 +0000

    Add quine code

--- sidKanren/quine.rkt
+++ sidKanren/quine.rkt
@@ -28,10 +28,21 @@
        (=/= x sym)
        (not-in-env sym resto))]))
 
+;; (define eval-expo
+;;   (lambda (expr env val)
+;;     (conde [(...ban ridiculous thing from env)
+;; 	    (conde symbolo
+;; 	      ...
+;; 	      )]
+
+
 (define eval-expo
   (lambda (expr env val)
     (conde
       [(symbolo expr) (lookupo expr env val)]
+      [(== expr `(quote ,val))
+       (absento 'closure val)
+       (not-in-env 'quote env)]
       [(fresh (x body)
          (== `(lambda (,x) ,body) expr)
          (== `(closure ,x ,body ,env) val)
@@ -39,10 +50,49 @@
          (not-in-env 'lambda env))]
       [(fresh (rator rand a env2 body x)
          (== `(,rator ,rand) expr)
+	 #|
+	 (not-in-env 'list env)
+	 (not-in-env 'list env2)
+	 |#
          (eval-expo rator env `(closure ,x ,body ,env2))
          (eval-expo rand env a)
-         (eval-expo body `((,x . ,a) . ,env2) val))])))
-;; ((lambda (x) (x x)) (lambda (x) (x x)))
+         (eval-expo body `((,x . ,a) . ,env2) val))]
+      [(fresh (e*)
+	 (== `(list ,e*) expr)
+	 (not-in-env 'list env)
+	 (absento 'closure e*)
+	 (mapo (lambda (x v) (eval-expo x env v)) e* val))])))
+
+(define (mapo g x* y*)
+  (conde
+    [(== x* empty) (== y* empty)]
+    [(fresh (x xs y ys)
+       (== x* (cons x xs))
+       (== y* (cons y ys))
+       (g x y)
+       (mapo g xs ys))]))
+; ((lambda (x) (x x)) (lambda (x) (x x)))
+
+; '(closure (x) x ()) (lambda (x) x) ()
+
+((lambda (x)
+   (list x (list (quote quote) x)))
+ (quote
+   (lambda (x)
+     (list x (list (quote quote) x)))))
+
+; ((lambda (x)
+;    `(,x (quote ,x)))
+;  (quote
+;    (lambda (x)
+;      `(,x (quote ,x)))))
+
+; synthesize a program such that
+; (P[i]) = (P[i-1], i, P[i+1])
+
+; (list x ('quote x))
 
 
 ;; (λ x (x x)) (λ x (x x))
+
+; (run 1 (p q) (. p q) (eval o p q) (eval o q p))
