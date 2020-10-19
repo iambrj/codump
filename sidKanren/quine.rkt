25ecc6271d70e4f09a68947e9860d09cdc362990
iambrj <joshibharathiramana@gmail.com>
2020-10-19 18:11:40 +0530

    Indent code, rename rest -> resto

--- sidKanren/quine.rkt
+++ sidKanren/quine.rkt
@@ -11,36 +11,44 @@
 
 ; symbolo q: enforce q is a symbol.
 ; absento x y: list x and symbol y
+; (define (lookupo id env val)
+;   (conde
+;     [(== env '()) (== #t #f)]
+;     [(fresh (x y resto)
+;        (== `((,x . ,y) . ,resto) env)
+;        (conde
+;          [(== x id) (== y val)]
+;          [(=/= x id) (lookupo id resto val)]))]))
+
 (define (lookupo id env val)
-  (conde
-    [(== env '()) (== #t #f)]
-    [(fresh (x y rest)
-      (== `((,x . ,y) . ,rest) env)
-      (conde
-	[(== x id) (== y val)]
-	[(=/= x id) (lookupo id rest val)]))]))
+  (fresh (x y resto)
+    (== `((,x . ,y) . ,resto) env)
+    (conde
+      [(== id x) (== val y)]
+      [(=/= id x) (lookupo id resto val)])))
+
 (define (not-in-env sym env)
   (conde
     [(== env '())]
-    [(fresh (x y rest)
-       (== `((,x . ,y) . ,rest) env)
+    [(fresh (x y resto)
+       (== `((,x . ,y) . ,resto) env)
        (=/= x sym)
-       (not-in-env sym rest))]))
+       (not-in-env sym resto))]))
 
 (define eval-expo
   (lambda (expr env val)
     (conde
       [(symbolo expr) (lookupo expr env val)]
       [(fresh (x body)
-	 (== `(lambda (,x) ,body) expr)
-	 (== `(closure ,x ,body ,env) val)
-       	 (symbolo x)
-	 (not-in-env 'lambda env))]
+         (== `(lambda (,x) ,body) expr)
+         (== `(closure ,x ,body ,env) val)
+         (symbolo x)
+         (not-in-env 'lambda env))]
       [(fresh (rator rand a env2 body x)
-	 (== `(,rator ,rand) expr)
-	 (eval-expo rator env `(closure ,x ,body ,env2))
-	 (eval-expo rand env a)
-	 (eval-expo body `((,x . ,a) . ,env2) val))])))
+         (== `(,rator ,rand) expr)
+         (eval-expo rator env `(closure ,x ,body ,env2))
+         (eval-expo rand env a)
+         (eval-expo body `((,x . ,a) . ,env2) val))])))
 ;; ((lambda (x) (x x)) (lambda (x) (x x)))
 
 
